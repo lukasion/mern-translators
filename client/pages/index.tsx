db499948ce52da9d3a3a4bd0776be8ddfcbe2ce0
Łukasz Fujarski <lukasion@gmail.com>
2024-05-05 20:47:15 +0200

    feat: Add NextUI

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import Image from "next/image";
 import {useState} from "react";
 import '@/styles/globals.css'
+import {Checkbox, NextUIProvider} from "@nextui-org/react";
+import Sidenav from "@/components/Sidenav";
 
 export default function Home() {
-    const [response, setResponse] = useState('test');
+    const [response, setResponse] = useState('');
 
     const checker = async () => {
         fetch("http://localhost:5000/create-user")
@@ -14,15 +15,11 @@ export default function Home() {
     }
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
-            <button
-                className="mt-8 px-6 py-3 text-white bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg pointer"
-                onClick={checker}
-            >
-                Check axios
-            </button>
-            {response}
-
-        </main>
+        <NextUIProvider>
+            <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
+                test
+                <Sidenav/>
+            </main>
+        </NextUIProvider>
     );
 }
