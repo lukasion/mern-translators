5e151d5de665665c877559aa4e43c1c10c105dc9
Łukasz Fujarski <lukasion@gmail.com>
2024-05-05 20:47:24 +0200

    feat: Create Sidenav component

---
+++ client/components/Sidenav.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import {Listbox, ListboxItem, ListboxSection} from '@nextui-org/react'
+import React from 'react'
+import {usePathname} from 'next/navigation'
+
+const Sidenav = () => {
+    const pathname = usePathname()
+
+    const navMenuItems = [
+        {label: 'Panel', href: '/'},
+        {label: 'Zlecenia', href: '/orders'},
+        {label: 'Użytkownicy', href: '/users'},
+        {label: 'Ustawienia', href: '/settings'},
+        {label: 'Kalendarz', href: '/calendar'},
+    ]
+
+    const isActive = (href: string) => {
+        if (href === '/') {
+            return pathname === '/'
+        }
+
+        if (pathname === null) {
+            return false
+        }
+
+        return pathname.includes(href)
+    }
+
+    return (
+        <aside className="hidden lg:block w-64 p-4 h-screen fixed top-16">
+            <Listbox
+                aria-label="User Menu"
+                // onAction={(key) => alert(key)}
+                className="p-0 gap-0 divide-y divide-default-300/50 dark:divide-default-100/80 bg-content1 max-w-[300px] overflow-visible shadow-small rounded-medium"
+                itemClasses={{
+                    base: 'px-3 first:rounded-t-medium last:rounded-b-medium rounded-none gap-3 h-12 data-[hover=true]:bg-default-100/80 data-[hover=true]:text-primary/80',
+                }}
+            >
+                <ListboxSection showDivider>
+                    {navMenuItems.slice(0, 4).map((item) => (
+                        <ListboxItem
+                            key={item.href}
+                            href={item.href}
+                            className={isActive(item.href) ? 'text-primary/90' : ''}
+                        >
+                            {item.label}
+                        </ListboxItem>
+                    ))}
+                </ListboxSection>
+                <ListboxSection>
+                    <ListboxItem
+                        key='logout'
+                        href='/logout'
+                    >
+                        Wyloguj się
+                    </ListboxItem>
+                </ListboxSection>
+            </Listbox>
+        </aside>
+    )
+}
+
+export default Sidenav
